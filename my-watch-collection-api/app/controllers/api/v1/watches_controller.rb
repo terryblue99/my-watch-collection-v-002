1e564be5b337a4e12b07a87b110ed155556b7030
terryblue99 <terryblue@comcast.net>
2019-07-09 14:44:44 -0500

    adde before_action and updated private section

--- my-watch-collection-api/app/controllers/api/v1/watches_controller.rb
+++ my-watch-collection-api/app/controllers/api/v1/watches_controller.rb
@@ -1,10 +1,20 @@
 module Api
     module V1
         class WatchesController < ApplicationController
+
+            before_action :set_watch, only: [:show, :edit, :update, :destroy]
+
             def index
+                render json: Watch.all
             end
 
             def show
+            end
+            
+            def create
+            end
+
+            def update
             end
 
             def destroy
@@ -12,6 +22,24 @@ module Api
 
             private
             def set_watch
+                @watch = Watch.find_watch(params[:id])
+            end
+
+            def watch_params
+                # params hash keys (strong params)
+                params.require(:watch).permit(
+                    :watch_name,
+                    :watch_maker,
+                    :movement,
+                    :band,
+                    :model_number,
+                    :water_resistance,
+                    :date_bought,
+                    :cost,
+                    :case_measurement,
+                    :watch_image,
+                    complications_attributes: [:complication_name, :complication_description]
+                )
             end
         end
     end
