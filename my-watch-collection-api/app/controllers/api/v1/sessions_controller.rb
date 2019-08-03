f7b2195bed03bc74dc8b23003da78b750d9910f6
terryblue99 <terryblue@comcast.net>
2019-08-02 20:15:02 -0500

    built the user model and sessions controller

---
+++ my-watch-collection-api/app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User
+            .find_by(email: params['user']['email'])
+            .try(:authenticate, params['user']['password']) # authenticate is built into rails
+
+    if user
+      session[:user_id] = user.user_id
+      render json: {
+        status: :created
+        logged_in: true
+        user: user
+      }
+    else
+      render json: { status: 401 } # code for unauthorised user
+    end
+  end
+end
--- my-watch-collection-api/db/schema.rb
+++ my-watch-collection-api/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_053351) do
+ActiveRecord::Schema.define(version: 2019_08_03_002957) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "watches", force: :cascade do |t|
     t.string "watch_name"
