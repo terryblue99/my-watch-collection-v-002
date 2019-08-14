f8f7ecee7f9f75967370b43ef9182a3489b54c6f
terryblue99 <terryblue@comcast.net>
2019-08-14 11:49:15 -0500

    added the logo to the dashboard

--- my-watch-collection-api/my-watch-collection-front/src/components/DashboardContent.js
+++ my-watch-collection-api/my-watch-collection-front/src/components/DashboardContent.js
@@ -1,12 +1,19 @@
-import React from 'react'
-
+// The following comment is required for @emotion to work
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core' // https://github.com/emotion-js/emotion'
+import logo from '../logo.jpg'
 
 const DashboardContent = (props) => {
   return (
-    <div >
-      DASHBOARD
+    <div css={css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}>
+      <img src={logo} alt='logo' align='middle' className='logo'/>
     </div>
   )
 }
+
 
 export default DashboardContent
