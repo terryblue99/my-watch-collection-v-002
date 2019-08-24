785ef1b9ca414b47647a686a5bb5ddefeb500c3b
terryblue99 <terryblue@comcast.net>
2019-08-24 12:03:18 -0500

    removed comment

--- my-watch-collection-api/app/models/watch.rb
+++ my-watch-collection-api/app/models/watch.rb
@@ -8,13 +8,7 @@ class Watch < ApplicationRecord
   def image_attached
     if !self.image.attached?
       self.image.attach(io: File.open(Rails.root.join('my-watch-collection-front', 'src', 'defaultWatchImage.png')), filename: 'defaultWatchImage.png' , content_type: 'image/png')
-    # else  
-    #   self.image.variant(resize: '300x300!').processed # ! & .processed forces a resize if needed
     end
   end
-
-  def image_resize
-
-  end  
-
+  
 end
