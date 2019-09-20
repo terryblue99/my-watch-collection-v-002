e669c8881c76d90f0b8ab702440a2993c524fa4d
terryblue99 <terryblue@comcast.net>
2019-09-19 21:16:39 -0500

    added comment

--- my-watch-collection-api/app/models/watch.rb
+++ my-watch-collection-api/app/models/watch.rb
@@ -6,6 +6,8 @@ class Watch < ApplicationRecord
   validate :image_attached
 
   def image_attached
+    # Checks if a watch image has been selected  
+    # and if not, a default image is stored
     if !self.image.attached?
       self.image.attach(io: File.open(Rails.root.join('my-watch-collection-front', 'src/images', 'defaultWatchImage.png')), filename: 'defaultWatchImage.png' , content_type: 'image/png')
     end
