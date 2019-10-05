fe12f8760464cb613b76352507099a963eb48c16
terryblue99 <terryblue@comcast.net>
2019-10-05 14:25:03 -0500

    added handleChange and handleSubmit for search to NavBar component

--- my-watch-collection-api/my-watch-collection-front/src/components/NavBar.js
+++ my-watch-collection-api/my-watch-collection-front/src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { Component } from 'react' 
 import { Navbar } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
@@ -6,25 +7,38 @@ import { NavLink } from 'react-router-dom'
 import { css, jsx } from '@emotion/core' // https://github.com/emotion-js/emotion
 import logoText from '../images/my-watch-collection-text.png'
 
-const NavBar = (props) => {
+class NavBar extends Component {
 
-  const handleSearch = (e) => {
-    alert(e.target.value)
+  state = {
+    searchText: ''
   }
-    
-  return (
-    <div className='Navbar'>
 
-      <Navbar css={css`
-        background-color: #454140;
-        margin: 0 auto;
-        padding-top: 5px;
-      `}>
-        <Navbar.Brand>
-         
-          <img src={logoText} alt='logo text' className='LogoText'/>
-       
-          <div className='Nav-media' css={css`
+  handleChange = (event) => {
+    this.setState({
+      searchText: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    alert('Submitting ' + this.state.searchText)
+  }
+  
+  render() {
+
+    return (
+      <div className='Navbar'>
+
+        <Navbar css={css`
+          background-color: #454140;
+          margin: 0 auto;
+          padding-top: 5px;
+        `}>
+          <Navbar.Brand> 
+            <img src={logoText} alt='logo text' className='LogoText'/>
+          </Navbar.Brand>
+
+          <div css={css`
               color: cornsilk;
               display: inline-block;
               font-size: 1rem;
@@ -39,7 +53,7 @@ const NavBar = (props) => {
                 padding-top: 15px;
               }
             `}>Logged in as:
-            <p className='Nav-media' css={css`
+            <p css={css`
               color: khaki;
               display: inline-block;
               font-size: 1rem;
@@ -52,67 +66,66 @@ const NavBar = (props) => {
               @media (max-width: 750px) {
                 padding-top: 15px;
               }
-            `}>{props.user.user.email}
+            `}>{this.props.user.user.email}
             </p>
           </div> 
           <div css={css`
-              color: cornsilk;
               display: inline-block;
               padding-left: 40px;
               margin-bottom: 3px;
             `}>
-            <form onSubmit={handleSearch}>
+            <form onSubmit={this.handleSubmit}>
               <input
-                  className='Nav-media'
-                  type='text'
-                  name='watch_search'
-                  placeholder='Search for a watch name or character string...'
-                  css={css`
-                    border-radius: 8px;
-                    margin-bottom: 5px;
-                    margin-right: 10px;
-                    min-width: 350px;
-                    padding: .25em;
+                type='text'
+                name='watch_search'
+                placeholder='Search for a watch name or character string...'
+                onChange={this.handleChange}
+                css={css`
+                  border-radius: 8px;
+                  margin-bottom: 5px;
+                  margin-right: 10px;
+                  min-width: 350px;
+                  padding: .25em;
 
-                    @media (min-width: 1500px) {
-                      font-size: 1.25rem
-                    }
+                  @media (min-width: 1500px) {
+                    font-size: 1rem
+                  }
 
-                    @media (max-width: 750px) {
-                      margin-top: 15px;
-                    }
-                  `}
+                  @media (max-width: 750px) {
+                    margin-top: 15px;
+                  }
+                `}
               />
               <button className='btn Search-button Button-text' type='submit'><b>Search</b></button>          
             </form>
           </div>
           <div>
-            {props.user.logged_in} {
-              <NavLink to='/logout'>
-                <span className='Nav-media' css={css`
-                  color: cornsilk;
-                  font-size: 1rem;
-                  position: absolute;
-                  right: 10px;
-                  top: 8px;
+            {this.props.user.logged_in} {
+              <NavLink  to='/logout'
+                        css={css`
+                        color: cornsilk;
+                        text-decoration: none;
+                        font-size: 1rem;
+                        position: absolute;
+                        right: 10px;
+                        top: 8px;
 
-                  &:hover {
-                    color: goldenrod;
-                    cursor: pointer;
-                  }
+                        &:hover {
+                          color: goldenrod;
+                          cursor: pointer;
+                        }
 
-                  @media (min-width: 1500px) {
-                    font-size: 1.25rem
-                  }
-                `}> Log Out
-                </span> 
+                        @media (min-width: 1500px) {
+                          font-size: 1.25rem
+                        }
+                `}> Log Out   
               </NavLink>
             }
-        </div>    
-        </Navbar.Brand>
-      </Navbar>
-    </div>
-  )
+          </div> 
+        </Navbar>
+      </div>
+    )  
+  }
 }
 
 const mapStateToProps = (state) => { 
