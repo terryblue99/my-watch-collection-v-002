1ff14ea5cdf2f51cb56cc61ee28716640a253393
terryblue99 <terryblue@comcast.net>
2019-11-09 18:31:00 -0600

    changed image.purge to image.purge_later

--- my-watch-collection-api/app/controllers/api/v2/watches_controller.rb
+++ my-watch-collection-api/app/controllers/api/v2/watches_controller.rb
@@ -24,7 +24,7 @@ class Api::V2::WatchesController < ApplicationController
     end
 
     def destroy
-        @watch.image.purge # synchronous
+        @watch.image.purge_later
         @watch.destroy
     end
 
