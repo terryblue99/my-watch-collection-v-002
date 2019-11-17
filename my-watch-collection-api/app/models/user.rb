7ea56cdab587f9b951b0bd6a626574aa3eaeb7a8
terryblue99 <terryblue@comcast.net>
2019-11-17 16:36:32 -0600

    added dependent: :destroy to has_many watches

--- my-watch-collection-api/app/models/user.rb
+++ my-watch-collection-api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :watches
+  has_many :watches, dependent: :destroy # dependent: :destroy causes all the associated watches
+                                         # to be destroyed when .destroy is used for the user
 
   validates_presence_of :email
   validates_uniqueness_of :email
