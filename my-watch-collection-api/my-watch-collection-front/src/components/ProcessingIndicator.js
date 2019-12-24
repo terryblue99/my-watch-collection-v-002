2ad82705250cb841b1785c3c5b13cd1e51edd891
terryblue99 <terryblue@comcast.net>
2019-12-24 17:18:13 -0600

    refactored

--- my-watch-collection-api/my-watch-collection-front/src/components/ProcessingIndicator.js
+++ my-watch-collection-api/my-watch-collection-front/src/components/ProcessingIndicator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { usePromiseTracker } from "react-promise-tracker"
 import Loader from 'react-loader-spinner'
 
-const ProcessingIndicator = props => {
+const ProcessingIndicator = () => {
   const { promiseInProgress } = usePromiseTracker()
   return promiseInProgress &&
     <div>
