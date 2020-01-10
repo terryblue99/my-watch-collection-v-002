cff3e73977657760f58e01f5642398005c7840a4
terryblue99 <terryblue@comcast.net>
2020-01-10 14:29:27 -0600

    use if statement for date validaton

--- my-watch-collection-api/my-watch-collection-front/src/components/DateValidation.js
+++ my-watch-collection-api/my-watch-collection-front/src/components/DateValidation.js
@@ -1,28 +1,22 @@
 const DateValidation = (date, type) => {
   // date validated for format yyyy-mm-dd, yyyy-mm, yyyy or 0
 
-  switch(type) {
-
-    case 'edit':
+  if (type === 'edit') {
       return (
         date.match(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/gm) ||
         date.match(/^\d{4}-(0[1-9]|1[012])$/gm) ||
         date.match(/^(19|20)\d{2}$/gm) 
       )
-
-    case 'add':
-
-      return (
-        date.match(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/gm) ||
-        date.match(/^\d{4}-(0[1-9]|1[012])$/gm) ||
-        date.match(/^(19|20)\d{2}$/gm)||
-        date.match(0) // For non-watch records.
-                      // Used when adding records that are not related to a specific watch.
-                      // For those records user must enter 'non-watch' in the Watch Name input.
-      )
-
+  } else if (type === 'add') {
+    return (
+      date.match(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/gm) ||
+      date.match(/^\d{4}-(0[1-9]|1[012])$/gm) ||
+      date.match(/^(19|20)\d{2}$/gm)||
+      date.match(0) // For non-watch records.
+                    // Used when adding records that are not related to a specific watch.
+                    // For those records user must enter 'non-watch' in the Watch Name input.
+    )
   }
-  
-}
+  }
 
 export default DateValidation
