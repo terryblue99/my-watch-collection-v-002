bcf30bb8d91c20872ec6a0b03b5248eaf56f2573
terryblue99 <terryblue@comcast.net>
2020-01-19 17:15:08 -0600

    amend text

--- my-watch-collection-api/my-watch-collection-front/src/components/NonWatchInfo.js
+++ my-watch-collection-api/my-watch-collection-front/src/components/NonWatchInfo.js
@@ -23,15 +23,8 @@ const NonWatchInfo = ()  => {
       <h3 className='NonWatch-text'>To add a non-watch record</h3>
       < br/> 
       <ol className='NonWatch-OL'>
-        <li>Click the <span className='Dark-red-color'>ADD WATCH</span> button</li> 
-        <li>Enter a title for it in the <span className='Dark-red-color'>Watch Maker</span> input</li> 
-        <li>Enter <span className='Dark-red-color'>non-watch</span> in the <span className='Dark-red-color'>Watch Name</span> input</li> 
-        <li>Enter <span className='Dark-red-color'>0</span> in the <span className='Dark-red-color'>Date Bought/Gifted</span> & <span className='Dark-red-color'>Cost</span> inputs</li>
-        <li>Click the <span className='Dark-red-color'>SAVE</span> button to save the record</li>
-        <li>Click the <span className='Dark-red-color'>BACK TO DASHBOARD</span> button to redisplay the dashboard</li>< br/>
-        
-        <li>Find and click on the non-watch from the list to display it</li>
-        <li>Click the <span className='Dark-red-color'>EDIT THIS NON-WATCH</span> button</li>
+        <li>Click the <span className='Dark-red-color'>ADD NON WATCH</span> button</li> 
+        <li>Enter a title for it in the <span className='Dark-red-color'>Non-Watch Title</span> input</li>
         <li>Use the <span className='Dark-red-color'>Notes</span> input and/or other input/s to enter information</li>
         <li>If an image is available, click the <span className='Dark-red-color'>Choose File</span> button to upload it</li>
         <li>Click the <span className='Dark-red-color'>SAVE</span> button to save the record</li>
