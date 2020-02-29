daeaf9ac4a4eaf826aeda6c8d27ddbae68ec9120
terryblue99 <terryblue@comcast.net>
2020-02-28 18:35:39 -0600

    modified

--- my-watch-collection-api/db/schema.rb
+++ my-watch-collection-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_193510) do
+ActiveRecord::Schema.define(version: 2020_02_06_160259) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
